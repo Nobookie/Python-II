9adfa6cff730123a62531bb9efead67cd32a7d9d
Nobookie <31146142+Nobookie@users.noreply.github.com>
2026-08-08 11:37:02 -0300

    Aula 04

---
+++ 04/todo_api/tasks/urls.py
@@ -0,0 +1,12 @@
+# Arquivo: tasks/urls.py
+
+from django.urls import path
+from .views import * 
+
+urlpatterns = [
+    # URL para listar e criar tarefas
+    path('tasks/', task_list_create_view, name='task-list-create'),
+    # URL para ver, atualizar ou deletar uma tarefa específica
+    path('tasks//', task_detail_view, name='task-detail'),
+]
+
